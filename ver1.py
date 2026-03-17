90290c8d8d55d2944231ac8aea66d0875510f114
danylomshovkoplias-beep <danylo.m.shovkoplias@ukd.edu.ua>
2026-03-17 17:05:57 +0000

    first version of program

--- ver1.py
+++ ver1.py
@@ -5,3 +5,4 @@ print(random_integer)
 random_integer2 = random.randint(6,10)
 print(random_integer2)
 print(f'{random_integer} {'+'} {random_integer2} {"="} {random_integer + random_integer2}')
+#first ver
